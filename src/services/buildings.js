fec0e57d31d56baa7b409e006abb3e2e494c4cb9
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-01-06 17:21:28 +0100

    refactor: services

--- src/services/buildings.js
+++ src/services/buildings.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { API_URL } from './api-config'
+import { API_URL } from './config'
 
 export function getBuildingIdentity() {
   return axios.get(`${API_URL}/building`)
