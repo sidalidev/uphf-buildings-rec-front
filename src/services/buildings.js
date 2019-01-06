1c409316208883657bc181bb0dfc4e5bc0863635
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-01-06 18:22:52 +0100

    feat(service): sending image to server and identifying it

--- src/services/buildings.js
+++ src/services/buildings.js
@@ -4,10 +4,5 @@ import { API_URL } from './config'
 export function getBuildingIdentity(imageToIdentify) {
   const formData = new FormData(imageToIdentify)
   formData.append('image', imageToIdentify)
-  console.log(imageToIdentify)
-  return axios.post(`${API_URL}/building`, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  })
+  return axios.post(`${API_URL}/building`, formData)
 }
